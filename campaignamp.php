d0be105ab7daf906f4f5daeaa2e1648db86b2664
Jim Seconde <jim.seconde@googlemail.com>
2016-05-03 20:28:13 +0100

    Finished task from campaignamp

--- campaignamp.php
+++ campaignamp.php
@@ -1,31 +1,51 @@
 <?php
 
-Print "CampaignAmp: Quick test from Alex\n";
-Print "For the following function, please give me an input:";
-$input = fgets(STDIN);
+function solution($input){
 
-//santatise input
+//santize input
+//regex
+$regex_pat = "#^[0-9]*$#i";
+$preg = preg_match($regex_pat, $input);
 
-$conv_input = intval($input);
+if ($preg == 0){
+  print "ERROR: Numbers only (and no floating points!), please" . PHP_EOL;
+  exit;
+}
 
-//convert fail or 0 isn't good
-if ($conv_input == 0)
-{
-  Echo "Error: Input must be a number or not 0!";
+//convert me to an integer
+$intinput = intval($input);
+
+//integer can't be over 1000
+if ($intinput > 1000){
+  print "ERROR: Input can't be over 1000" . PHP_EOL;
   Exit;
 }
 
-var_dump($conv_input);
+//loop start
+$i = 1;
 
-//no floats please
-
-if (is_float($conv_input)) {
-    echo "is float\n";
-} else {
-    echo "is not float\n";
+//start loop
+while($i <= $intinput)
+{
+  //outputstring to build text result (if there is one)
+  $outputstring = "";
+  if ($i %3 == 0){$outputstring = "Fizz";}
+  if ($i %5 == 0){$outputstring = $outputstring . "Buzz";}
+  if ($i %7 == 0){$outputstring = $outputstring . "Woof";}
+  //print either outputstring or integer
+  if ($outputstring == ""){
+    echo $i . PHP_EOL;
+  } else {
+    echo $outputstring . PHP_EOL;
+  }
+  //logic loop ending
+  $i++;
+}
+Print PHP_EOL . "COMPLETED" . PHP_EOL;
 }
 
-function theTest(){
-
-}
- ?>
+//Start running code from here:
+print "Give me an input:" . PHP_EOL; //Used PHP_EOL as Mac coded
+$datain = fgets(STDIN);
+solution($datain);
+?>
