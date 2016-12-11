22c425567ed763305169bbdb469504b7f93089c3
Jim Seconde <jim.seconde@googlemail.com>
2016-12-11 22:06:04 +0000

    Not bad, 4 groups not 3 

--- brims_name_regex.php
+++ brims_name_regex.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Regex Testing: Reformat Names</br>";
+echo "Regex Testing: Reformat Names" . PHP_EOL;
 
 $inputs = [
   'Jim Seconde',
@@ -8,9 +8,9 @@ $inputs = [
   'J K Seconde',
   'Mr Jim Seconde',
   'Mr. Jim Seconde'
-]
+];
 
-$pattern = "/^(Mr|Mrs|Dr|Ms|Miss|Prof|Rev)?\.?\b(.+)\b(.+)$/";
+$pattern = "^(Mr|Mrs|Dr|Ms|Miss|Prof|Rev)?(\.)?\s*(\S+)\s+(\S+)$@";
 
 $i = 1;
 
@@ -18,6 +18,9 @@ foreach ($inputs as $string) {
   preg_match($pattern, $string, $matches);
   $trimmedResult = array_map('trim', $matches);
 
-  echo "Case Number: " . $i;
-  $i++
+  echo "Case Number: " . $i . PHP_EOL;
+  echo "Input String: " . $string . PHP_EOL;
+  echo "Dump Match Array" . PHP_EOL;
+  var_dump($trimmedResult);
+  $i++;
 }
