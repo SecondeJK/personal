84cfb110e04a4ddaf3e1a8f9faa39f7815dfee60
Jim Seconde <jim.seconde@googlemail.com>
2016-12-30 15:23:58 +0000

    Starting functions

---
+++ Python/functions.py
@@ -0,0 +1,6 @@
+def number_comparison(importNumber):
+    print('Welcome to this function, called number_comparison')
+    print('You input the following number: %s' % (str(importNumber))
+
+importNumber = input('Give me a number')
+number_comparison(importNumber)
