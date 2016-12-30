d40ec09c84cd2694ea9473c13b82328887044f97
Jim Seconde <jim.seconde@googlemail.com>
2016-12-30 14:07:50 +0000

    Expanding main script

--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -1,1 +1,23 @@
-print("Hello World, James Seconde comes to Python")
+from datetime import datetime
+
+print("Time to practise Python!")
+
+variableOne = 2
+print('Print me a variable: ' + str(variableOne))
+variableTwo = 36 * variableOne
+print (str(variableTwo))
+
+'''
+name = input("What is your name?")
+quest = input("What is your quest?")
+color = input("What is your favorite color?")
+
+print('Ah, so your name is %s, your quest is %s, ' \
+"and your favorite color is %s." % (name, quest, color))
+'''
+
+now = datetime.now()
+print('Starting some date functions. Here is the current date: ' + str(now))
+print('And here is the year: ' + str(now.year))
+print('And here is the month: ' + str(now.month))
+print('And here is the day: ' + str(now.day))
