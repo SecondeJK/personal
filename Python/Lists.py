1830eb7112b47324b233dacfa8740ca074643e1d
Jim Seconde <jim.seconde@googlemail.com>
2017-01-01 22:53:40 +0000

    More array stuff

--- Python/Lists.py
+++ Python/Lists.py
@@ -1,4 +1,18 @@
 print('Some array actions now, defined in Python as lists and dictionaries')
 
 myArray = ['Birmingham', 'Manchester', 'Bristol', 'Portsmouth']
-print myArray
+print('Starting list: ' + str(myArray))
+
+#Right, time to add somethimg
+myArray.append('Nottingham')
+print('Appended a new city: ' + str(myArray))
+
+#Give me an index
+print('Give me the index of Bristol: ' + str(myArray.index('Bristol')))
+
+#Overwrite index for Portsmouth
+myArray.insert(3, "Glasgow")
+print('Insert new city at index 3, here you go: ' + str(myArray))
+
+for city in myArray:
+    print("In this for loop, the city is %s" % city)
