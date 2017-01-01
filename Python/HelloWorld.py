76fe58d95ea2e3ac396dc45919d3b876a38204fb
Jim Seconde <jim.seconde@googlemail.com>
2017-01-01 16:11:23 +0000

    Added some string wordplay and did some debugging

--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -22,4 +22,18 @@ print('And here is the year: ' + str(now.year))
 print('And here is the month: ' + str(now.month))
 print('And here is the day: ' + str(now.day))
 
-print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+print('%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second))
+
+# Some string wordplay from Code Academy
+pyg = 'ay'
+
+original = input('Enter a word:')
+word = original.lower()
+first = word[0]
+new_word = word + first + pyg
+new_word = new_word[1:len(new_word)]
+
+if len(original) > 0 and original.isalpha():
+    print(new_word)
+else:
+    print('empty')
